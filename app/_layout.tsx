e2b2dff3e8fc4eb748cb8a588506f81689165007
Fede Aletti <fedaletti@gmail.com>
2025-08-05 13:27:51 -0300

    Refactor layout and remove unused components. Updated RootLayout to include splash screen handling and theme color management. Deleted NotFound, TabLayout, and Explore components to streamline the app structure. Adjusted color constants for improved theme support.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,53 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useEffect } from 'react';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({}, 'background');
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
   if (!loaded) {
-    // Async font loading only occurs in development.
     return null;
   }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: {
+            backgroundColor: 'pink',
+          },
+          headerStyle: {
+            backgroundColor: 'pink',
+          },
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: 'Inicio' }} />s
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
