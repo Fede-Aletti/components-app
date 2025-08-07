95f06b9266a850fba15526daefb88d1f1490757c
Fede Aletti <fedaletti@gmail.com>
2025-08-07 11:31:13 -0300

    Implement pan gesture animation in Animation102Screen. Added Animated.View for draggable functionality using PanResponder, allowing the box to be moved and reset on release. Updated styles for container and box to enhance layout and appearance.

--- app/animation-102/index.tsx
+++ app/animation-102/index.tsx
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native';
+import { useRef } from 'react';
+import { Animated, PanResponder, StyleSheet, View } from 'react-native';
 
 const Animation102Screen = () => {
+
+  const panGesture = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      {
+        dx: panGesture.x,
+        dy: panGesture.y
+      }
+    ], {
+      useNativeDriver: false,
+    }),
+    onPanResponderRelease: () => {
+      Animated.spring(panGesture, {
+        toValue: { x: 0, y: 0 },
+        useNativeDriver: false,
+      }).start();
+    }
+  });
+
   return (
-    <View>
-      <Text>Animation102Screen</Text>
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[panGesture.getLayout(), styles.box]}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#61DAFB',
+    borderRadius: 10,
+  },
+});
+
 export default Animation102Screen;
